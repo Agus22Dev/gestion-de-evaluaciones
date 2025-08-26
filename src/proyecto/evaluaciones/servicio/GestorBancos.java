1aa94cb0beaca3adfcb9e2400aae7e680a8f901f
Leon suhr <go56wux@mytum.de>
2025-08-27 01:30:22 +0200

    1.5/1.6 implementados

--- src/proyecto/evaluaciones/servicio/GestorBancos.java
+++ src/proyecto/evaluaciones/servicio/GestorBancos.java
@@ -2,39 +2,43 @@ package proyecto.evaluaciones.servicio;
 
 import proyecto.evaluaciones.dominio.BancoPreguntas;
 import proyecto.evaluaciones.dominio.Pregunta;
+import proyecto.evaluaciones.dominio.Tema;
 
 import java.util.*;
 
 public class GestorBancos {
-    private final Map<String, BancoPreguntas> bancos = new LinkedHashMap<>();
+    private final Map<Tema, BancoPreguntas> bancos = new LinkedHashMap<>();
 
-    public boolean crearBanco(String nombreBanco) {
-        String key = nombreBanco.trim();
-        if (bancos.containsKey(key)) return false;
-        bancos.put(key, new BancoPreguntas(key));
+    public boolean crearBanco(Tema tema) {
+        if (bancos.containsKey(tema)) return false;
+        bancos.put(tema, new BancoPreguntas(tema.getNombre(), tema));
         return true;
     }
 
-    public Optional<BancoPreguntas> obtenerBanco(String nombreBanco) {
-        return Optional.ofNullable(bancos.get(nombreBanco));
+    public Optional<BancoPreguntas> obtenerBanco(Tema tema) {
+        return Optional.ofNullable(bancos.get(tema));
     }
 
-    public List<String> listarNombresBancos() {
+    public List<Tema> listarTemasBancos() {
         return new ArrayList<>(bancos.keySet());
     }
 
-    public void agregarPregunta(String nombreBanco, Pregunta p) {
-        BancoPreguntas banco = bancos.computeIfAbsent(nombreBanco, BancoPreguntas::new);
+    public void agregarPregunta(Tema tema, Pregunta p) {
+        BancoPreguntas banco = bancos.computeIfAbsent(tema, t -> new BancoPreguntas(t.getNombre(), t));
         banco.agregarPregunta(p);
     }
 
-    public List<Pregunta> listarPreguntas(String nombreBanco) {
-        BancoPreguntas b = bancos.get(nombreBanco);
+    public List<Pregunta> listarPreguntas(Tema tema) {
+        BancoPreguntas b = bancos.get(tema);
         return (b == null) ? Collections.emptyList() : b.getPreguntas();
     }
 
-    public Map<String, BancoPreguntas> verBancos() {
+    public Map<Tema, BancoPreguntas> verBancos() {
         return Collections.unmodifiableMap(bancos);
     }
-}
 
+    public boolean eliminarPregunta(Tema tema, String idPregunta) {
+        BancoPreguntas b = bancos.get(tema);
+        return (b != null) && b.eliminarPreguntaPorId(idPregunta);
+    }
+}
